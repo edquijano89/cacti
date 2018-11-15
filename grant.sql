095cc0f8776a1e5bf36804fba841cc86df193aa1
edquijano89 <43075042+edquijano89@users.noreply.github.com>
2018-11-15 09:14:20 -0600

    Update grant.sql

--- grant.sql
+++ grant.sql
@@ -1,1 +1,5 @@
 GRANT SELECT ON mysql.time_zone_name to 'usuario'@'localhost' IDENTIFIED BY '**Un1tec**'
+grant CREATE,INSERT,SELECT,DELETE,UPDATE on cactidb.* to usuario@localhost;
+grant CREATE,INSERT,SELECT,DELETE,UPDATE on cactidb.* to usuario;
+SET PASSWORD FOR usuario@localhost = old_password('**Un1tec**');
+flush privileges;
